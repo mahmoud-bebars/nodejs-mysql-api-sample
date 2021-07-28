8631cb8a78b80b467a60c55de2fcd37ad8730c55
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2021-07-28 13:29:44 +0200

    Update programmingLanguages.js

--- services/programmingLanguages.js
+++ services/programmingLanguages.js
@@ -18,68 +18,6 @@ async function getMultiple(page = 1){
   }
 }
 
-async function create(programmingLanguage){
-  const result = await db.query(
-    `INSERT INTO programming_languages 
-    (name, released_year, githut_rank, pypl_rank, tiobe_rank) 
-    VALUES 
-    (?, ?, ?, ?, ?)`, 
-    [
-      programmingLanguage.name, programmingLanguage.released_year,
-      programmingLanguage.githut_rank, programmingLanguage.pypl_rank,
-      programmingLanguage.tiobe_rank
-    ]
-  );
-
-  let message = 'Error in creating programming language';
-
-  if (result.affectedRows) {
-    message = 'Programming language created successfully';
-  }
-
-  return {message};
-}
-
-async function update(id, programmingLanguage){
-  const result = await db.query(
-    `UPDATE programming_languages 
-    SET name=?, released_year=?, githut_rank=?, 
-    pypl_rank=?, tiobe_rank=? 
-    WHERE id=?`, 
-    [
-      programmingLanguage.name, programmingLanguage.released_year,
-      programmingLanguage.githut_rank, programmingLanguage.pypl_rank,
-      programmingLanguage.tiobe_rank, id
-    ]
-  );
-
-  let message = 'Error in updating programming language';
-
-  if (result.affectedRows) {
-    message = 'Programming language updated successfully';
-  }
-
-  return {message};
-}
-
-async function remove(id){
-  const result = await db.query(
-    `DELETE FROM programming_languages WHERE id=?`, 
-    [id]
-  );
-
-  let message = 'Error in deleting programming language';
-
-  if (result.affectedRows) {
-    message = 'Programming language deleted successfully';
-  }
-
-  return {message};
-}
-
 module.exports = {
   getMultiple,
-  create,
-  update,
-  remove
 }
