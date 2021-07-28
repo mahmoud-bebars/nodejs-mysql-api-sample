1bf81df4686a284be9be0f2a3380f2785c30c9c4
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2021-07-28 13:28:29 +0200

    Update programmingLanguages.js

--- routes/programmingLanguages.js
+++ routes/programmingLanguages.js
@@ -11,35 +11,3 @@ router.get('/', async function(req, res, next) {
     next(err);
   }
 });
-
-/* POST programming language */
-router.post('/', async function(req, res, next) {
-    try {
-      res.json(await programmingLanguages.create(req.body));
-    } catch (err) {
-      console.error(`Error while creating programming language`, err.message);
-      next(err);
-    }
-  });
-
-/* PUT programming language */
-router.put('/:id', async function(req, res, next) {
-    try {
-      res.json(await programmingLanguages.update(req.params.id, req.body));
-    } catch (err) {
-      console.error(`Error while updating programming language`, err.message);
-      next(err);
-    }
-  });
-
-  /* DELETE programming language */
-router.delete('/:id', async function(req, res, next) {
-    try {
-      res.json(await programmingLanguages.remove(req.params.id));
-    } catch (err) {
-      console.error(`Error while deleting programming language`, err.message);
-      next(err);
-    }
-  });
-
-module.exports = router;
